282eaecd0f46013788cd62a1bc441d6b646ca855
marinagrebneva <sokovets@outlook.com>
2022-11-02 09:14:51 +0100

    fixedc calculations.js file

--- calculations.js
+++ calculations.js
@@ -10,16 +10,16 @@ function decreaseBalance(x, y) {
 }
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;//можно добавить новую переменную, чтобы не перезаписывать значение переменной х (зависит от назначения функции)
-  //console.log("x") вывод строки, содержащей только символ х 
-  console.log(`x = ${x}`)
+  x = x / y; //можно добавить новую переменную, чтобы не перезаписывать значение переменной х (зависит от назначения функции)
+  //console.log("x") вывод строки, содержащей только символ х
+  console.log(`x = ${x}`);
   return x;
   //console.log("x"); данная строчка не будет выполняться т.к. выход происходит после return, перенесена выше return
 }
 
 function getRestAfterDivision(x, y) {
- // x = x % y; перезапишет переменную х, правильнее завести новую переменную
- let rest = x % y;
+  // x = x % y; перезапишет переменную х, правильнее завести новую переменную
+  let rest = x % y;
   return rest;
 }
 
@@ -27,4 +27,3 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2)); //функция принимает два аргемента, третий лишний, либо должна быть изменена функция
 console.log(getRestAfterDivision(7000, 3));
-
